fcc356d8adaf8cf6cbf0112cd2333970464b55db
Gilbert Nordhammar <gilbertnordhammar@gmail.com>
2020-04-26 20:49:17 +0200

    section 2-4 (only code from section 2)

---
+++ section-2/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var timesClicked = 0
+        btnClickMe.setOnClickListener {
+            timesClicked++
+            textView.text = timesClicked.toString()
+            Toast.makeText(this@MainActivity, "Hi Gilbert!.", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
